55f76388c7a87b49b742d93f7f022ffb9859fd24
hamid <hamid.shafikani@gmail.com>
2018-08-30 07:25:32 +0200

    csc

---
+++ src/main/java/com/hamid/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.hamid.service;
+
+import com.hamid.model.Product;
+import com.hamid.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+
+/** 
+ * Service layer.
+ * Specify transactional behavior and mainly
+ * delegate calls to Repository.
+ */
+@Service
+public class ProductService {
+
+
+}
